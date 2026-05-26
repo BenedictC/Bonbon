154fc449a5131cdcaefe5fa2cb6c570b206a7f36
Benedict Cohen <ben@benedictcohen.co.uk>
2026-05-26 11:41:25 +0100

    Adds UINavigationController init that takes a delegate

--- Sources/UICandy/Controllers/UINavigationController+Initializer.swift
+++ Sources/UICandy/Controllers/UINavigationController+Initializer.swift
@@ -3,6 +3,18 @@ import UIKit
 
 public extension UINavigationController {
 
+    convenience init(delegate: UINavigationControllerDelegate?) {
+        self.init()
+        self.delegate = delegate
+    }
+
+    convenience init(delegate: UINavigationControllerDelegate?, @ArrayBuilder<UIViewController> controllers stackBuilder: () -> [UIViewController]) {
+        let stack = stackBuilder()
+        self.init()
+        self.setViewControllers(stack, animated: false)
+        self.delegate = delegate
+    }
+
     convenience init(@ArrayBuilder<UIViewController> controllers stackBuilder: () -> [UIViewController]) {
         let stack = stackBuilder()
         self.init()
